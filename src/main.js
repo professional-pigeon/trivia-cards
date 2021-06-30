ed8fc5e4b9489a3ffd61468aa4c6d5bb0e50473c
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-30 11:09:27 -0700

    create service logic for getting photos of Mars
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    Co-Authored-By: Kevin Funez <kevin.funez315@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import MartianPics from './martianService.js'
+
+
+function getMartianPhoto(response) {
+  if(response) {
+    
+  }
+}
+body.photos[0].img_src
+
+$(document).ready(function() {
+
+})
---
+++ src/martianService.js
@@ -0,0 +1,14 @@
+export default class MartianPics {
+  static getMartianPhoto() {
+    return fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=200&camera=MAST&api_key=${process.env.API_KEY}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+  }
+}
